19fe41b600fc8bc8d52f2d11e9fbd4a711bb4b28
Can Karabulut <can@cankarabulut.com.tr>
2023-05-05 20:21:15 +0300

    changing name and icon

--- canDeneme/ContentView.swift
+++ canDeneme/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "trash")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Merhaba Can")
         }
         .padding()
     }
