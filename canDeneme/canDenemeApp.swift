bdec82853d8e5728a4241f583b7185e6473f0989
Can Karabulut <can@cankarabulut.com.tr>
2023-05-05 20:07:25 +0300

    Initial Commit

---
+++ canDeneme/canDenemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  canDenemeApp.swift
+//  canDeneme
+//
+//  Created by Can Karabulut on 5.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct canDenemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
